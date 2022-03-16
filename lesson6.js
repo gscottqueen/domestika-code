44b0f951ace656182c50eac4fbb661298d585564
gscottqueen <geoffreysqueen@gmail.com>
2022-03-16 11:00:56 -0400

    lesson 6

--- lesson6.js
+++ lesson6.js
@@ -1,14 +1,56 @@
-const canvasSketch = require('canvas-sketch');
+import { canvasSketch } from 'canvas-sketch';
+import { random } from 'canvas-sketch-util'
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 1080, 1080 ]
 };
 
-const sketch = () => {
+const sketch = ({ context, width, height }) => {
+
+  const agents = [];
+
+  for (let i = 0; i < 40; i++) {
+    const x = random.range(0, width);
+    const y = random.range(0, height);
+    agents.push(new Agent(x, y))
+  }
+
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    agents.forEach(agent => {
+      agent.draw(context);
+    });
   };
 };
 
 canvasSketch(sketch, settings);
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Agent {
+  constructor(x, y) {
+    this.pos = new Point(x, y);
+    this.radius = 10;
+  }
+
+  draw(context) {
+    context.fillStyle = 'white';
+    context.beginPath();
+    context.arc(
+        this.pos.x,
+        this.pos.y,
+        this.radius,
+        0,
+        Math.PI * 2
+      );
+    context.fillStyle = 'black';
+    context.fill();
+  }
+}
